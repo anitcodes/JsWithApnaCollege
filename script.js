8f9310b841bc3c85eb1ea7442cff2eb0793c0e52
Anit Shrestha <anitshrestha101@gmail.com>
2025-08-01 19:43:36 +0545

    chap1,2,3 done

---
+++ script.js
@@ -0,0 +1,119 @@
+// comment in JS
+// Part of code which is not executed
+
+/* This is multi line
+   comment in js. */
+
+// chapter 1:- Variable and Data Types in JS
+
+// //creating object in js
+
+// let student = {
+//     name: "Anit",
+//     level: "Bachelor",
+//     roll: 2,
+//     isPass: true
+// };
+
+// //accessing the value from key and changing the value of it
+// student.name = "Mamata"
+// console.log(student.name)
+
+// qn1. create a const object called "product" to store iformation shown in the picture.
+/*
+const product = {
+    name: "Ball Pen",
+    rating: 4,
+    price: 270
+};
+*/
+
+// Chapter 2:- Operators and Conditional Statements
+
+// Arithmetic Operator
+// let a = 5
+// let b = 2
+
+// console.log("a + b = ", a+b)
+// console.log("a - b = ", a-b)
+// console.log("a * b = ", a*b)
+// console.log("a / b = ", a/b)
+// console.log("a % b = ", a%b) //modulus
+// console.log("a ** b = ", a**b) //5^2(Exponentiation)
+
+//Unary Operator Types:
+// 1. Pre-increment and decrement
+// 2. Post-increment and decrement
+
+// let a = 5
+// a++;
+// console.log(a);
+
+//Assignment Operator
+
+// let a = 5
+// let b = 2
+
+// a += 4; //a = a+4
+// console.log(a) //9
+
+//Conparision Operator
+
+// let a = 5
+// let b = 2
+// console.log(a == b) //true
+// console.log(a != b)//false
+// console.log(a === b)
+// console.log(a !== b)
+
+//Logical Operators
+
+// 1. Logical AND &&
+// 2. Logical OR ||
+// 3. Logical NOT
+
+// let a = 2
+// let b = 4
+
+// let cond1 = a<b; //true
+// let cond2 = a==2;//true
+// console.log(cond1 && cond2);// true
+
+//Conditional Statements
+
+// qn. write a code which can give grades to students according to their scores:
+// 80-100, A
+// 70-89, B 
+// 60-69, C
+// 50-59, D
+// 0-49, F
+
+// let marks = prompt("Enter your marks:");
+// marks = Number(marks); // Convert input to number
+
+// if(marks >= 80 && marks <= 100){
+//     alert("You have scored A Grade");
+// }
+// else if(marks >= 70 && marks < 80){
+//     alert("You have scored B Grade");
+// }
+// else if(marks >= 60 && marks < 70){
+//     alert("You have scored C Grade");
+// }
+// else if(marks >= 50 && marks < 60){
+//     alert("You have scored D Grade");
+// }
+// else if(marks >= 0 && marks < 50){
+//     alert("You have scored F Grade");
+// }
+// else{
+//     alert("Invalid input");
+// }
+
+
+// Chapter 3:- Loops in JS
+
+
+
+// Chapter 6:- DOM 
+
