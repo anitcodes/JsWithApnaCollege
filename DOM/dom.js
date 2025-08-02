e9ace78dc712fa55a3a34c72bf083d8c94cf7578
Anit Shrestha <anitshrestha101@gmail.com>
2025-08-02 09:40:27 +0545

    dom manipulation

---
+++ DOM/dom.js
@@ -0,0 +1,19 @@
+// window.document.body.style.background = "black"
+// window.document.body.style.color = "grey"
+
+//DOM(Document Object Model) Manipulation
+
+// let div = document.getElementsByClassName("myClass")
+// console.dir(div)
+
+// let para = document.getElementsByTagName("p")
+// console.dir(para)
+
+// let firstElm = document.querySelector("p");
+// console.dir(firstElm);
+
+// let allElm = document.querySelectorAll("p");
+// console.dir(allElm);
+
+// let button = document.getElementById("btn");
+// console.dir(button)
